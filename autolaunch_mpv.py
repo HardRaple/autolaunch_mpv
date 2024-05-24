888948f2ce3ed6bf1829f9a125de9c28bb5b71f3
HardRaple <nikkucher82@gmail.com>
2024-05-24 12:39:13 +0300

    Update autolaunch_mpv.py

--- autolaunch_mpv.py
+++ autolaunch_mpv.py
@@ -7,7 +7,7 @@ number = randint(0, len(urllist)-1)
 url = urllist[number]
 tm = str(120 + randint(1, 100))
 
-system('sleep '+tm+'; \
+system('sleep '+tm+'m; \
     amixer sset Master on -D pulse; \
     amixer sset Master 40% -D pulse; \
     DISPLAY=:0 mpv --fullscreen '+url)
